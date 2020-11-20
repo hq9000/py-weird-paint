9caf3ff54091ae8f2dd94662963e71ca44049e06
Sergey Grechin <grechin.sergey@gmail.com>
2020-11-20 11:06:28 +0300

    proxy canvas elaboration

--- py_weird_paint/proxy_canvas.py
+++ py_weird_paint/proxy_canvas.py
@@ -1,6 +1,14 @@
 from tkinter import Canvas
+from typing import Optional
+
+from line_style import LineStyle
 
 
 class ProxyCanvas:
     def __init__(self, canvas: Canvas):
         self._canvas = canvas
+        self._last_x: Optional[int] = None
+        self._last_y: Optional[int] = None
+
+    def draw_line(self, x1: int, y1: int, x2: int, y2: int, style: LineStyle, delay: int = 0):
+        pass
